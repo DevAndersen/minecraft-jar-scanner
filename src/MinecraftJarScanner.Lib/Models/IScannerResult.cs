cc2009bc1d744655c0f5aef4c62b0b27b1114ce6
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2025-11-16 23:34:28 +0100

    Fixed missing polymorphic JSON serialization

--- src/MinecraftJarScanner.Lib/Models/IScannerResult.cs
+++ src/MinecraftJarScanner.Lib/Models/IScannerResult.cs
@@ -1,5 +1,10 @@
-﻿namespace MinecraftJarScanner.Lib.Models;
+﻿using System.Text.Json.Serialization;
 
+namespace MinecraftJarScanner.Lib.Models;
+
+[JsonDerivedType(typeof(ScannerResultError), typeDiscriminator: nameof(ScannerResultError))]
+[JsonDerivedType(typeof(ScannerResultJarFile), typeDiscriminator: nameof(ScannerResultJarFile))]
+[JsonDerivedType(typeof(ScannerResultZipFile), typeDiscriminator: nameof(ScannerResultZipFile))]
 public interface IScannerResult
 {
     string FullPath { get; }
